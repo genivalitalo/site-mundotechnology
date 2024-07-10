73cb35cea39a5d48547888071592e826e3d96d92
genivalitalo <italobsilva2108@gmail.com>
2024-07-10 17:18:37 -0300

    Criação de Header e Menu

---
+++ js/script.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const btnMenu = document.getElementById('btn_menu');
+    const dropBar = document.querySelector('.container_links_drop');
+
+    btnMenu.addEventListener('click', function() {
+        // Alternar entre os ícones
+        if (btnMenu.classList.contains('bx-menu-alt-right')) {
+            btnMenu.classList.remove('bx-menu-alt-right');
+            btnMenu.classList.add('bx-menu'); // Novo Item
+        } else {
+            btnMenu.classList.remove('bx-menu');
+            btnMenu.classList.add('bx-menu-alt-right'); // Retornar ao ícone original
+        }
+        
+        // Alternar a visibilidade do nav com efeito de entrada
+        if (dropBar.classList.contains('show')) {
+            dropBar.classList.remove('show');
+        } else {
+            dropBar.classList.add('show');
+        }
+
+    });
+});
